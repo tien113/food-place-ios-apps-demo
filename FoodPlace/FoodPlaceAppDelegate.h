06a08f6c109faea7ccf4bb6fcd0fcf04bf1641f7
vo <tien113@gmail.com>
2012-02-26 14:28:35 +0200

    Initial Commit

---
+++ FoodPlace/FoodPlaceAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FoodPlaceAppDelegate.h
+//  FoodPlace
+//
+//  Created by vo on 2/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FoodPlaceAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FoodPlace/FoodPlaceFirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FoodPlaceFirstViewController.h
+//  FoodPlace
+//
+//  Created by vo on 2/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FoodPlaceFirstViewController : UIViewController
+
+@end
