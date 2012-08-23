f455d2531bbdb610fcd8c9093fbe2e0899054681
vo <tien113@gmail.com>
2012-08-23 16:19:39 +0300

    // [self.tableView reloadData]; // reload data tableView.

--- FoodPlace/LocationTableViewController.m
+++ FoodPlace/LocationTableViewController.m
@@ -113,7 +113,7 @@
 - (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocatio
 {
     [self sortPlacesByDistanceFrom:newLocation]; // distance from places
-    [self.tableView reloadData]; // reload data tableView
+    // [self.tableView reloadData]; // reload data tableView
     [manager stopUpdatingLocation]; // stop updating location  
 }
 
